2cffce713d2bf1c04194d6d8e716519ba4f8d548
Haniel Austria <hanniiel@live.com.mx>
2023-01-28 16:42:41 -0600

    test without express middleware

--- server/api/v1/idol/.get.ts
+++ server/api/v1/idol/.get.ts
@@ -1,3 +1,9 @@
 import { defineEventHandler } from 'h3'
+import { Idol } from '~~/server/models/idol'
+import connect from '../../../../server/connection'
 
-export default defineEventHandler((event) => 'v1 nice get ')
+connect()
+export default defineEventHandler(async (event) => {
+    let idols = await Idol.find({}, {}, { skip: 0 * 10, limit: 20 }).exec()
+    return idols
+})
--- server/routers/idol.ts
+++ server/routers/idol.ts
@@ -1,12 +1,11 @@
 import { Request, Router } from 'express'
-import { Idol } from '../models/idol'
+
 const router = Router()
 
 router.route('/idol').get(async (req, res) => {
     //
     try {
-        let idols = await Idol.find({}, {}, { skip: 0 * 10, limit: 20 }).exec()
-        res.status(200).send(idols)
+        res.send({ succes: true })
     } catch (e) {
         res.status(400).send({ error: e })
     }
